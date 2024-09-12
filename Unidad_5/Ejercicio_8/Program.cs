6be954c97252f5eb5e602d48008db021a60a2947
Eduardo AlvareZ <udealvarez@icloud.com>
2024-09-12 11:27:17 -0300

    Unidad 5 - Ejercicio 8

---
+++ Unidad_5/Ejercicio_8/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Ejercicio_8;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // EJERCICIO
+
+        /*
+            Hacer un programa que solicite 10 números y 
+            luego emita cuántos de ellos son números pares.
+        */
+
+        int numero, contador = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+                contador++;
+        }
+        Console.WriteLine("la cantidad de numeros pares es: " + contador);
+    }
+}
