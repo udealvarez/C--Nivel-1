f5f122288a5e69d9cef0c11da372515de95dc6f8
Eduardo AlvareZ <udealvarez@icloud.com>
2024-09-12 11:40:03 -0300

    Unidad 5 - Ejercicio 10

---
+++ Unidad_5/Ejercicio_10/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ejercicio_10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // EJERCICIO
+
+        /*
+            Hacer un programa que solicite 12 temperaturas y luego muestre
+            Cuántas de ellas están por encima de los 30 grados.
+        */
+
+        int numero, contador = 0;
+
+        for (int i = 0; i < 12; i++)
+        {
+            Console.WriteLine("Ingresar la temperatura: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero > 30)
+                contador++;           
+        }
+
+        Console.WriteLine("La cantidad de TEMPERATURAS mayores a 30 es: " + contador);  
+    }
+}
