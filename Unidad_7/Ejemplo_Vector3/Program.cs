29193befdeb8b1f72bc8170c593503c669f0eb80
Eduardo AlvareZ <udealvarez@icloud.com>
2024-10-08 11:21:33 -0300

    Unidad 7 - Ejemplo de VECTORES con booleanos

---
+++ Unidad_7/Ejemplo_Vector3/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ejemplo_Vector3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // VECTORES CON BOOL
+
+        // los vectores que conocemos son INT, DOUBLE, BOOL, CHAR
+
+        int[] legajos = new int[100];
+        bool[] asistencia = new bool[100];
+
+        for (int i = 0; i < 16; i++)
+        {
+            for (int x = 0; x < 100; x++)
+            {
+                // aca puedo jugar con 16 lotes de 100 personas cada uno
+            }
+        }
+        
+
+
+
+
+        
+        
+
+    }
+}
